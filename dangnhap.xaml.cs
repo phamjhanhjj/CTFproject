c1e257633f684cc73f6b83c7438b580a29539e54
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2025-03-27 03:58:35 +0700

    Add project files.

---
+++ dangnhap.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTFproject
+{
+    /// <summary>
+    /// Interaction logic for dangnhap.xaml
+    /// </summary>
+    public partial class dangnhap : Window
+    {
+        public dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void login_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void quenmatkhau_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void register_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
