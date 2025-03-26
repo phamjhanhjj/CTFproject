195b31767f62c09d93bb47c63f563409253b8b30
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2025-03-27 03:58:35 +0700

    Add project files.

---
+++ dangky.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CTFproject
+{
+    /// <summary>
+    /// Interaction logic for dangky.xaml
+    /// </summary>
+    public partial class dangky : Page
+    {
+        public dangky()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void register_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void login_bt_Click(object sender, RoutedEventArgs e)
+        {
+            var dangnhap = new dangnhap();
+            dangnhap.Show();
+        }
+    }
+}
---
+++ dangnhap.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTFproject
+{
+    /// <summary>
+    /// Interaction logic for dangnhap.xaml
+    /// </summary>
+    public partial class dangnhap : Window
+    {
+        public dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void login_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void register_bt_Click(object sender, RoutedEventArgs e)
+        {
+            dangky registrationWindow = new dangky();
+            Window window = new Window
+            {
+                Content = registrationWindow,
+                Title = "Registration"
+            };
+            window.Show();
+            this.Hide();
+        }
+
+        private void forgot_bt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
